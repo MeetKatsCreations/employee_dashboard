9a1a1459c59be06d88ff942739c1d24e7190198f
Anshika2608 <anshika2210142@akgec.ac.in>
2025-04-29 21:56:02 +0530

    task_assigned_api_done

---
+++ Server/Controllers/taskController.js
@@ -0,0 +1,37 @@
+const Task = require('../Models/taskModel');
+const User = require('../Models/userModel');
+
+const assignTask = async (req, res) => {
+  const { title, description, dueDate, assignedTo } = req.body;
+  const assignedBy = req.userId;
+  const role=req.role;
+  if(role !== "admin"){
+    return res.status(404).json({message:"Employee can not assign tasks."})
+  }
+
+  if (!assignedTo || !title || !description || !dueDate) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  try {
+    const employee = await User.findById(assignedTo);
+    if (!employee || employee.role !== 'employee') {
+      return res.status(404).json({ message: 'Employee not found or invalid role' });
+    }
+
+    const task = new Task({
+      title,
+      description,
+      dueDate,
+      assignedTo,
+      assignedBy,
+    });
+
+    await task.save();
+    res.status(201).json({ message: 'Task assigned successfully', task });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+module.exports={assignTask}
